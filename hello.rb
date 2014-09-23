b050b18f56b284ca48e6c011f59f623aa938319d
Sachiko Shamoto <sshamoto@users.noreply.github.com>
2014-09-23 06:44:22 +0000

    Rollback cause something is wrong

--- hello.rb
+++ hello.rb
@@ -7,13 +7,3 @@ end
 get '/aiit' do
   "Welcome to AIIT!"
 end
-
-set(:probability) { |value| condition { rand <= value } }
-
-get '/practice', :probalibity => 0.1 do
-  "Lucky day!"
-end
-
-get '/practice' do
-  "Unlucky day!"
-end
