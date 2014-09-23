da426c9dc4ecf171a8e5bea9ff6ed09745a3557c
Sachiko Shamoto <sshamoto@users.noreply.github.com>
2014-09-23 06:32:55 +0000

    Add feature - tell lucky or unlucky randomly

--- hello.rb
+++ hello.rb
@@ -7,3 +7,11 @@ end
 get '/aiit' do
   "Welcome to AIIT!"
 end
+
+set(:probability) { |value| condition { rand <= value } }
+get '/practice', :probalibity => 0.1 do
+  "Lucky day!"
+end
+get '/practice' do
+  "Unlucky day!"
+end
